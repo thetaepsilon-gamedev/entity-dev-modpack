5b1958a54451f9d5f1a269db388f23be5735d2e7
thetaepsilon-gamedev <thetaepsilon-gamedev@noreply.users.github.com>
2018-07-08 13:13:42 +0100

    drag_physics: [new code] add cbox_grind helper utilities

---
+++ drag_physics/lib/physics/cbox_grind_util.lua
@@ -0,0 +1,24 @@
+--[[
+helpers for the cbox_grind module (see appropriate lua file).
+]]
+
+local i = {}
+
+-- reference implementation of mkcube which just assigns the result into a table.
+local mkcube_collect = function(sxmin, symin, szmin, sxmax, symax, szmax)
+	local r = {}
+
+	r.xmin = sxmin
+	r.ymin = symin
+	r.zmin = szmin
+
+	r.xmax = sxmax
+	r.ymax = symax
+	r.zmax = szmax
+end
+i.mkcube_collect = mkcube_collect
+
+
+
+return i
+
