9b471d0ca46756a4d8fb0323758398a9afe4ec3b
thetaepsilon-gamedev <thetaepsilon-gamedev@noreply.users.github.com>
2018-07-08 18:04:23 +0100

    drag_physics: minetest natives: [new code] provide friction sampler wrapping minetest.get_node and minetest.registered_nodes

---
+++ drag_physics/natives/minetest/physics/mt_default_friction_calc.lua
@@ -0,0 +1,24 @@
+--[[
+In minetest-specific code: provide definition lookup and node sampling,
+based on minetest.registered_nodes and .get_node(), respectively.
+]]
+local mk_frictionf = mtrequire("ds2.minetest.drag_physics.friction_sampler_mt_def")
+local mk_sampler = mtrequire("ds2.minetest.drag_physics.friction_sampler_impl")
+
+-- throwaway allocations, whyyyyyy
+local pos = {}
+local getnodename = function(x, y, z)
+	pos.x = x
+	pos.y = y
+	pos.z = z
+	local node = minetest.get_node(pos)
+	return node.name
+end
+
+local nlookup = function(n) return minetest.registered_nodes[n] end
+local frictionf = mk_frictionf(getnodename, nlookup)
+
+local i = {}
+i.friction_sampler = mk_sampler(frictionf)
+return i
+
